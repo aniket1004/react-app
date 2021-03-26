fcc72bc4207d82a1f00ec98b11be75dc0113cfbe
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-26 11:50:40 +0530

    Updated

---
+++ App.js
@@ -0,0 +1,14 @@
+//import AdminNavbar from './AdminNavbar';
+import AdminNavbar from './AdminNavbar';
+
+import './App.css';
+
+
+function App() {
+  return (
+      <div>
+        <AdminNavbar />
+     </div>
+  );
+}
+export default App;
