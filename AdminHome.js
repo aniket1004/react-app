eece2b2e6dcd5ddc4d9540724d3a8b993b358f99
aniket1004 <aniketdhole1004@gmail.com>
2021-03-26 10:56:34 +0530

    adding src

---
+++ AdminHome.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import axios from 'axios';
+
+class AdminHome extends React.Component 
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {name : '', email : '',message : ''};
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleName(event)
+    {
+        this.setState({ name : event.target.value});
+    }
+    handleEmail(event)
+    {
+        this.setState({ email : event.target.value});
+    }
+    handleSubmit(event)
+    {
+        event.preventDefault();
+        const userData = {
+            name : this.state.name,
+            email : this.state.email
+        };
+        axios.post('http://localhost/php-api/controller/php_api/user/create_user.controller.php',userData)
+        .then(response => {
+            this.setState({message : 'User created..',name : '', email : ''});
+        })
+        .catch(error => {
+            this.setState({message : 'Unable to create user'});
+        })
+    }
+    render()
+    {
+        const { name , email} = this.state;
+        return (
+            <div className="container mt-3 p-5">
+                <h1 className="mt-5 text-danger text-center">Add User</h1>
+                
+                <form onSubmit={this.handleSubmit} className="mt-4">
+                    <div className="form-outline mb-4">
+                        <label className="form-label text-danger" for="name">Name</label>
+                        <input 
+                            type="text" 
+                            id="name" 
+                            name="name" 
+                            className="form-control" 
+                            onChange={this.handleName} 
+                            value={name} />
+                    </div>
+                    <div className="form-outline mb-4">
+                        <label className="form-label text-danger" for="email">Email Id</label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            className="form-control" 
+                            onChange={this.handleEmail} 
+                            value={email}/>
+                    </div>
+                    <button type="submit" className="btn btn-danger btn-block mb-4">Insert</button>
+                </form>
+                <p className="text-danger">{this.state.message}</p>
+            </div>
+        )
+    }
+}
+
+export default AdminHome;
---
+++ UserDetails.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import Loader from './Loader';
+
+class UserDetails extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            users : [],
+            message : '',
+            loading : true
+        };
+        this.loadData = this.loadData.bind(this);
+    }
+    async loadData()
+    {
+        await fetch('http://localhost/php-api/view/readall.view.php')
+        .then(response => response.json())
+        .then(data => { 
+            this.setState({ users : data,loading : false});
+        }    
+        );
+    }
+    async componentDidMount(){
+        await fetch('http://localhost/php-api/view/readall.view.php')
+        .then(response => response.json())
+        .then(data => { 
+            this.setState({ users : data,loading : false});
+        }    
+        );
+    }
+    async deleteUser(uid,mail,e)
+    {
+        const url = `http://localhost/php-api/controller/php_api/user/delete_user.controller.php?id=${uid}&email=${mail}`;
+        await fetch(url)
+        .then(response => response.json())
+        .then(data => this.setState({message : data.Message}));
+        this.loadData();
+    }
+    render()
+    {
+        if (this.state.loading)
+        {
+            return (
+                <Loader />
+            )
+        }
+        else
+        {
+        if (this.state.users !== null)
+        {
+        return(
+            
+            <div className="container ms-2 mt-4 ">
+                <h1 className="text-danger">View User Details</h1>
+                <table className="table align-middle mt-4 table-bordered">
+                    <thead>
+                        <tr className="table-info">
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {   
+                            this.state.users.map(user => {
+                            
+                            return(
+                            <tr>
+                                <td className="text-info">{user.id}</td>
+                                <td className="text-info">{user.name}</td>
+                                <td className="text-info">{user.email}</td>
+                                <td><i class="fas fa-trash fa-2x text-danger" onClick={(e)=> this.deleteUser(user.id,user.email,e)}></i></td>
+                            </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            
+        )
+        }
+        else
+        {
+            return (<h1 style={{
+                textAlign : 'center',
+                marginTop : '15%'
+             }} className="text-danger">Data not found</h1>)
+        }
+    }
+    }
+}
+
+export default UserDetails;
