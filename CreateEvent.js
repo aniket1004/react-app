d681c7f1c6038a39dbefb5190ebc177fac443c25
aniket1004 <aniketdhole1004@gmail.com>
2021-03-26 12:30:39 +0530

    adding event form

--- CreateEvent.js
+++ CreateEvent.js
@@ -13,7 +13,33 @@ class CreateEvent extends React.Component
     render()
     {
         return(
-            <h1>Create Events</h1>
+            <div className="container">
+                <h1 className="mt-5 text-danger text-center">Add Event</h1>
+                
+                <form onSubmit="" className="mt-4">
+                    <div className="form-outline mb-4">
+                        <label className="form-label text-danger" for="name">Event</label>
+                        <input 
+                            type="text" 
+                            id="event" 
+                            name="event" 
+                            className="form-control" 
+                            onChange="" 
+                            value="" />
+                    </div>
+                    <div className="form-outline mb-4">
+                        <label className="form-label text-danger" for="email">Description</label>
+                        <input 
+                            type="text" 
+                            id="desc" 
+                            className="form-control" 
+                            onChange="" 
+                            value="" />
+                    </div>
+                    <button type="submit" className="btn btn-danger btn-block mb-4">Insert</button>
+                </form>
+                <p className="text-danger"></p>
+            </div>
         )
     }
 }
