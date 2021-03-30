948fde801f35e2439bb935db638ac95f2371ac0f
aniket1004 <aniketdhole1004@gmail.com>
2021-03-30 16:02:08 +0530

    read event

--- ReadEvent.js
+++ ReadEvent.js
@@ -33,6 +33,7 @@ class ReadEvent extends React.Component
     }
     async deleteEvent(id,e)
     {
+        this.setState({message : 'Loading...'});
         const url = `http://localhost/php-api/controller/php_api/event/delete_event.controller.php?id=${id}`;
         await fetch(url)
         .then(response => response.json())
