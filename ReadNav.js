b8fdb4e75aa09ccdbce0034b0b040cf80c46dab2
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-09 12:36:31 +0530

    Added read serices nav item

--- ReadNav.js
+++ ReadNav.js
@@ -3,29 +3,35 @@ import UserDetails from './UserDetails';
 import ReadEvent from './ReadEvent';
 import ReadBlog from './ReadBlog';
 import ReadCategory from './ReadCategory';
+import ReadServices from './ReadService';
 
 const CreateNav = (props) => {
     return (
 <div className="container mt-4">
     <Router>        
     <ul className="nav nav-pills mb-3" id="ex1" role="tablist">
-    <li className="nav-item" role="presentation">
-        <Link to="/readUser" className="nav-link bg-info text-dark">User</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/readUser" className="nav-link bg-info text-dark">User</Link>
+        </li>
     <div>
-    <li className="nav-item" role="presentation">
-        <Link to="/readEvent" className="nav-link bg-info text-dark">Event</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/readEvent" className="nav-link bg-info text-dark">Event</Link>
+        </li>
     </div>
     <div>
-    <li className="nav-item" role="presentation">
-        <Link to="/readBlog" className="nav-link bg-info text-dark">Blog</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/readBlog" className="nav-link bg-info text-dark">Blog</Link>
+        </li>
     </div>
     <div>
-    <li className="nav-item" role="presentation">
-        <Link to="/readCategory" className="nav-link bg-info text-dark">Category</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/readCategory" className="nav-link bg-info text-dark">Category</Link>
+        </li>
+    </div>
+    <div>
+        <li className="nav-item" role="presentation">
+            <Link to="/readServices" className="nav-link bg-info text-dark">Services</Link>
+        </li>
     </div>
     {/* <li class="nav-item" role="presentation">
         
@@ -36,6 +42,7 @@ const CreateNav = (props) => {
         <Route exact component={ReadEvent} path="/readEvent"></Route>
         <Route exact component={ReadBlog} path="/readBlog"></Route>
         <Route exact component={ReadCategory} path="/readCategory"></Route>
+        <Route exact component={ReadServices} path="/readServices"></Route>
     </Switch>
     </Router>
 </div>
