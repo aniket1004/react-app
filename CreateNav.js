971efd481d74de830495e4fea7d5022bfff6a593
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-12 11:17:54 +0530

    Swapnil added add service menu item

--- CreateNav.js
+++ CreateNav.js
@@ -4,29 +4,34 @@ import CreateUser from './CreateUser';
 import CreateEvent from './CreateEvent';
 import CreateBlog from './CreateBlog';
 import CreateCategory from './CreateCategory';
-
+import CreateService from './CreateService';
 const CreateNav = (props) => {
     return (
 <div className="container mt-4">
     <Router>        
     <ul className="nav nav-pills">
-    <li className="nav-item" role="presentation">
-        <Link to="/createUser" className="nav-link bg-info text-dark">User</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/createUser" className="nav-link bg-info text-dark">User</Link>
+        </li>
     <div>
-    <li className="nav-item" role="presentation">
-        <Link to="/createEvent" className="nav-link bg-info text-dark">Event</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/createEvent" className="nav-link bg-info text-dark">Event</Link>
+        </li>
     </div>
     <div>
-    <li className="nav-item" role="presentation">
-        <Link to="/createBlog" className="nav-link bg-info text-dark">Blog</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/createBlog" className="nav-link bg-info text-dark">Blog</Link>
+        </li>
     </div>
     <div>
-    <li className="nav-item" role="presentation">
-        <Link to="/createCategory" className="nav-link bg-info text-dark">Category</Link>
-    </li>
+        <li className="nav-item" role="presentation">
+            <Link to="/createCategory" className="nav-link bg-info text-dark">Category</Link>
+        </li>
+    </div>
+    <div>
+        <li className="nav-item" role="presentation">
+            <Link to="/createService" className="nav-link bg-info text-dark">Service</Link>
+        </li>
     </div>
     {/* <li class="nav-item" role="presentation">
         
@@ -37,6 +42,7 @@ const CreateNav = (props) => {
         <Route exact component={CreateEvent} path="/createEvent"></Route>
         <Route exact component={CreateBlog} path="/createBlog" />
         <Route exact component={CreateCategory} path="/createCategory" />
+        <Route exact component={CreateService} path="/createService" />
     </Switch>
     </Router>
 </div>
